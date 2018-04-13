d241cc37315850c5fb466895d09790ca09eff946
yellowzz <1217114421@qq.com>
2018-04-13 11:22:07 +0800

    Create 1004.py

---
+++ Basic/1004.py
@@ -0,0 +1,10 @@
+n = int (input())
+L = []
+for i in range(n):
+    a = input().split(' ')
+    L = L + a
+max_num = max(list(map(int,L[2::3])))
+min_num = min(list(map(int,L[2::3])))
+
+print (L[L.index(str(max_num)) - 2] + ' ' + L[L.index(str(max_num)) - 1])
+print (L[L.index(str(min_num)) - 2] + ' ' + L[L.index(str(min_num)) - 1])

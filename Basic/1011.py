e47dc4e5593ce090efa7f445e897f14429716e5b
yellowzz <1217114421@qq.com>
2018-07-03 18:24:59 +0800

    Update 1011.py

--- Basic/1011.py
+++ Basic/1011.py
@@ -1,3 +1,4 @@
+#B1011.py
 T = int ( input())
 jg = []
 for i in range(1, T + 1):
@@ -6,5 +7,24 @@ for i in range(1, T + 1):
         jg.append('true')
     else:
         jg.append('false')
-for i in range(1, T + 1):
     print ( 'Case #' + str(i) + ': ' + jg[i-1])
+    
+----------------------------------------------------------------------------------------------------------------------------
+//B1011.cpp
+#include <stdio.h>
+int main(void)
+{
+	int T ,tcase = 1;
+	scanf("%d",&T);
+	while(T--)
+	{
+		long long A,B,C;
+		scanf("%lld%lld%lld",&A,&B,&C);
+		if (A + B > C)
+			printf("Case #%d: true\n",tcase++);
+		else
+			printf("Case #%d: false\n",tcase++);
+	}
+
+	return 0;
+}

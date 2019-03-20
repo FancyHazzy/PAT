6caf05cf77cd2ab816061b1a9902e0fc04b56941
yellowzunzhi <1217114421@qq.com>
2019-03-20 10:24:54 +0800

    Update B1088.cpp

--- Basic/B1088.cpp
+++ Basic/B1088.cpp
@@ -1,6 +1,13 @@
+//模拟方程计算。一直答案错误，找了很久才发现是for里面再次int jia，由于定义域不同，所以两个jia值不一样。
+
+
 #include<iostream>
 #include<cmath>
 using namespace std;
+const string out1="Cong";
+const string out2="Ping";
+const string out3="Gai";
+const string out4="No Solution";
 
 int main (void)
 {
@@ -9,12 +16,29 @@ int main (void)
     double bing;
 
     cin >> M >> X >> Y;
-    for (int i = 99;i > 9;i-- ){
-        yi = (jia % 10 * 10 + x % 10);
-        bing = abs(jia - yi) / (double)(X);
+    for (jia = 99;jia > 9;jia-- ){
+        yi = ((jia % 10) * 10 + jia / 10);
+        bing = (double)(abs(jia - yi)) / X;
         if (yi == Y * bing)
+        	break;
     }
 
+    if (jia == 9)
+    	cout << out4;
+    else {
+    	cout << jia << " ";
+    	if (M == jia) cout << out2 << " ";
+    	else if (M < jia) cout << out1 << " ";
+    	else cout << out3 << " ";
+
+    	if (M == yi) cout << out2 << " ";
+    	else if (M < yi) cout << out1 << " ";
+    	else cout << out3 << " ";
+
+    	if (M == bing) cout << out2;
+    	else if (M < bing) cout << out1;
+    	else cout << out3;    	
+    } 
 
 
 

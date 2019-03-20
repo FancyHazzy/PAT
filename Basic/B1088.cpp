6eac60e303300ca259d2c82186ef5c2320202738
yellowzunzhi <1217114421@qq.com>
2019-03-20 08:35:44 +0800

    Create B1088.cpp

---
+++ Basic/B1088.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main (void)
+{
+    //模拟方程
+    int jia, yi, M, X, Y;
+    double bing;
+
+    cin >> M >> X >> Y;
+    for (int i = 99;i > 9;i-- ){
+        yi = (jia % 10 * 10 + x % 10);
+        bing = abs(jia - yi) / (double)(X);
+        if (yi == Y * bing)
+    }
+
+
+
+
+    return 0;
+}

07a9e619e433d9167f04b5a16e21b1692ee2d9f5
yellowzunzhi <1217114421@qq.com>
2019-03-22 17:36:25 +0800

    Update 1004.py

--- Basic/1004.py
+++ Basic/1004.py
@@ -8,3 +8,4 @@ min_num = min(list(map(int,L[2::3])))
 
 print (L[L.index(str(max_num)) - 2] + ' ' + L[L.index(str(max_num)) - 1])
 print (L[L.index(str(min_num)) - 2] + ' ' + L[L.index(str(min_num)) - 1])
+#
